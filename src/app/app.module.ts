4631abe83c60269a5054d26e268ecd8be6b3fb85
Damaso Flores <damaso.flores@gmail.com>
2020-05-26 22:32:31 -0500

    making progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseHttpInterceptor } from './core/services/base-https.interceptor';
 import { AngularSharedModule } from './shared/angular-shared.module';
+import { HomeComponent } from './home/home.component';
+import { RegistryComponent } from './registry/registry.component';
+import { ShowerComponent } from './shower/shower.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, RegistryComponent, ShowerComponent],
   imports: [
     // Core
     CommonModule,
