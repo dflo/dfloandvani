4545f4a8b15fa01972ea362a4eefae85f576a548
Damaso Flores <damaso.flores@gmail.com>
2020-07-10 00:17:54 -0500

    add ngx-translate-router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,11 +31,8 @@ export class AppComponent implements OnInit {
     private appMediaService: AppMediaService,
     private translate: TranslateService,
   ) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('es');
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('es');
+    // translate.setDefaultLang('es');
+    // translate.use('es');
   }
 
   ngOnInit(): void {
